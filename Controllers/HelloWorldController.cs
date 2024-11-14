1bfb980ab00236515160ec9634cfe8139f061444
Agon Jakupi <jakupigoni123@hotmail.com>
2024-11-14 03:39:53 +0100

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        // GET: /api/helloworld
+        [HttpGet]
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
